8f9d9fd20cdd6c60bfd0b676cfae7899f7348967
ravaka34 <ravakanyaina34@gmail.com>
2024-02-17 22:52:36 +0300

    connecting to the mongoDB

---
+++ magneva/config/db.config.js
@@ -0,0 +1,9 @@
+// TODO get the uri from process.env
+
+const password = 'RavakaNyAina_34';
+const uri = `mongodb+srv://ravakanyaina34:${password}@cluster0.kdqazsw.mongodb.net/magneva?retryWrites=true&w=majority`;
+
+
+module.exports = {
+    uri : uri
+}
