8a244d139885b0e29f0c741dcbddedb2068a597c
ravaka34 <ravakanyaina34@gmail.com>
2024-02-20 18:16:25 +0300

    opening Hour Model + appointment model

--- magneva/models/index.js
+++ magneva/models/index.js
@@ -9,7 +9,7 @@ db.uri = dbConfig.uri;
 
 db.user = require("./user.model.js");
 db.role = require("./role.model.js")
-
+db.openingHour = require("./openingHour.model.js");
 
 db.ROLES = ["user", "employee", "admin"]
 
